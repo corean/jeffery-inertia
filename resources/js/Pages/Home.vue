c817e447927fdf5a0338d055590f721f757e4e99
corean <corean@corean.biz>
2022-12-13 21:36:41 +0900

    - props

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+    <ul v-for="(item, index) in frameworks" :key="index">
+        <li>{{ item }}</li>
+    </ul>
+</template>
+
+<script setup>
+defineProps({
+    name : String,
+        frameworks : Array,
+})
+</script>
+
+<!--
+<script>
+export default {
+    props : {
+        name : String,
+    }
+}
+</script>
+-->
