159a0f76e68b95660da49567d5891f0b3fa7809d
corean <corean@corean.biz>
2022-12-15 00:04:23 +0900

    - An Important SPA Security Concern

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -7,8 +7,18 @@
     />
   </Head>
   <h1 class="text-2xl">Users</h1>
+
+  <ul>
+    <li v-for="user in users" key="user.id" v-html="user.name"></li>
+  </ul>
 </template>
 
 <script setup>
 import { Head } from '@inertiajs/inertia-vue3'
+defineProps({
+  users: {
+    type: Array,
+    default: [],
+  },
+})
 </script>
